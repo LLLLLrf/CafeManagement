7086eb0e3962ba4fd25a0db9abc7b5180ef281d8
LLLLLrf <1626226046@qq.com>
2022-10-10 14:10:20 +0800

    new

--- cfm-vue/src/components/CartPage.vue
+++ cfm-vue/src/components/CartPage.vue
@@ -1,9 +1,9 @@
 <template>
-    <div style="">
+    <div class="layout">
         <div class="menu-name">购物车详情</div>
         <hr style="background:#2F3CF4;height:2px;margin-left: 160px;"/>
         <el-row>
-            <el-col :span="10" style="font-size:4em;color:#2F3CF4;font-weight:900;">ORDER</el-col>
+            <el-col :span="10" style="font-size:4em;color:#2F3CF4;font-weight:900;margin-left: 40px;">ORDER</el-col>
         </el-row>
     </div>
 </template>
@@ -22,9 +22,10 @@ data() {
 <style>
 .menu-name{
 text-align: left;
-width: 100%;
-margin-left: 200px;
 font-weight: bolder;
 }
 
+.layout{
+    margin-left: 0px;
+}
 </style>
