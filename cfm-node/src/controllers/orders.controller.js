cbd0fe3dfb8bad7720f80d4243b7936f907c2552
FaterYU <1042277145@qq.com>
2022-12-01 01:14:45 +0800

    DESC by updatedAt->orders

--- cfm-node/src/controllers/orders.controller.js
+++ cfm-node/src/controllers/orders.controller.js
@@ -40,7 +40,7 @@ exports.create = (req, res) => {
 exports.findAll = (req, res) => {
     const name = req.body.name;
     var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
-    Orders.findAll({ where: condition })
+    Orders.findAll({ where: condition , order: [['updatedAt', 'DESC']]})
       .then(data => {
         res.send(data)
       })
