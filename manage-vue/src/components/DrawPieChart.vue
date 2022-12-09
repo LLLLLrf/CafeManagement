8a2ed387c570e089f5189d025156af7ac52b17f8
LLLLLrf <1626226046@qq.com>
2022-12-09 09:52:09 +0800

    change statistcis page

--- manage-vue/src/components/DrawPieChart.vue
+++ manage-vue/src/components/DrawPieChart.vue
@@ -31,7 +31,7 @@ import * as echarts from 'echarts'
             
             const option = {
                 title: {
-                    text: '本日收入',
+                    text: 'Daily Income',
                     left: 'center'
                 },
                 tooltip: {
@@ -41,7 +41,7 @@ import * as echarts from 'echarts'
                 legend: {
                     orient: 'vertical',
                     left: 'left',
-                    data: ['coffee', 'fruit', 'milk']
+                    data: ['coffee', 'tea', 'soda']
                 },
                 series: [
                     {
@@ -51,8 +51,8 @@ import * as echarts from 'echarts'
                     center: ['50%', '60%'],
                     data: [
                         { value: 335, name: 'coffee' },
-                        { value: 310, name: 'fruit' },
-                        { value: 234, name: 'milk' },
+                        { value: 310, name: 'tea' },
+                        { value: 234, name: 'soda' },
                     ],
                     emphasis: {
                         itemStyle: {
